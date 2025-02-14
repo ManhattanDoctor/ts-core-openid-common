d29ab1fa2e3e2b41b9dae9316ee805abec38cae7
Renat Gubaev <gubaev.r@n-t.io>
2025-02-14 13:25:13 +0300

    auto commit

--- src/lib/IOpenIdToken.ts
+++ src/lib/IOpenIdToken.ts
@@ -1,3 +1,90 @@
+import { DateUtil, TransformUtil } from '@ts-core/common';
+import { OpenIdTokenInvalidError, OpenIdTokenUndefinedError } from '../error';
+import * as _ from 'lodash';
+
+export class OpenIdToken<H extends IOpenIdTokenHeader = IOpenIdTokenHeader, C extends IOpenIdTokenContent = IOpenIdTokenContent> implements IOpenIdToken {
+    // --------------------------------------------------------------------------
+    //
+    //  Properties
+    //
+    // --------------------------------------------------------------------------
+
+    protected _value: string;
+
+    protected _header: H;
+    protected _signed: string;
+    protected _content: C;
+    protected _signature: Buffer;
+
+    // --------------------------------------------------------------------------
+    //
+    //  Constructor
+    //
+    // --------------------------------------------------------------------------
+
+    constructor(value: string) {
+        if (_.isNil(value)) {
+            throw new OpenIdTokenUndefinedError();
+        }
+        this._value = value;
+        this.commitTokenProperties();
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Protected Methods
+    //
+    // --------------------------------------------------------------------------
+
+    protected commitTokenProperties(): void {
+        try {
+            let array = this.value.split('.');
+            this._signed = `${array[0]}.${array[1]}`;
+            this._header = TransformUtil.toJSON(Buffer.from(array[0], 'base64').toString());
+            this._content = TransformUtil.toJSON(Buffer.from(array[1], 'base64').toString());
+            this._signature = Buffer.from(array[2], 'base64');
+        } catch (error) {
+            throw new OpenIdTokenInvalidError(error.message);
+        }
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Public Properties
+    //
+    // --------------------------------------------------------------------------
+
+    public get value(): string {
+        return this._value;
+    }
+
+    public get header(): H {
+        return this._header;
+    }
+
+    public get signed(): string {
+        return this._signed;
+    }
+
+    public get content(): C {
+        return this._content;
+    }
+
+    public get signature(): Buffer {
+        return this._signature;
+    }
+
+    public get isExpired(): boolean {
+        return this.content.exp * DateUtil.MILLISECONDS_SECOND < Date.now();
+    }
+}
+
 export interface IOpenIdToken {
     value: string;
+}
+
+export interface IOpenIdTokenHeader { }
+
+export interface IOpenIdTokenContent {
+    exp: number;
 }
--- src/service/keycloak/KeycloakToken.ts
+++ src/service/keycloak/KeycloakToken.ts
@@ -1,91 +1,17 @@
-import { DateUtil, TransformUtil } from '@ts-core/common';
-import { OpenIdTokenInvalidError, OpenIdTokenUndefinedError } from '../../error';
-import { IOpenIdToken } from '../../lib';
+import { IOpenIdTokenContent, IOpenIdTokenHeader, OpenIdToken } from '../../lib';
 import * as _ from 'lodash';
 
-export class KeycloakToken implements IOpenIdToken {
-    // --------------------------------------------------------------------------
-    //
-    //  Properties
-    //
-    // --------------------------------------------------------------------------
+export class KeycloakToken extends OpenIdToken<IKeycloakTokenHeader, IKeycloakContent> { }
 
-    protected _value: string;
-
-    protected _header: IOpenIdTokenHeader;
-    protected _signed: string;
-    protected _content: IOpenIdTokenContent;
-    protected _signature: Buffer;
-
-    // --------------------------------------------------------------------------
-    //
-    //  Constructor
-    //
-    // --------------------------------------------------------------------------
-
-    constructor(value: string) {
-        if (_.isNil(value)) {
-            throw new OpenIdTokenUndefinedError();
-        }
-        this._value = value;
-        this.commitTokenProperties();
-    }
-
-    // --------------------------------------------------------------------------
-    //
-    //  Protected Methods
-    //
-    // --------------------------------------------------------------------------
-
-    protected commitTokenProperties(): void {
-        try {
-            let array = this.value.split('.');
-            this._signed = array[0] + '.' + array[1];
-            this._header = TransformUtil.toJSON(Buffer.from(array[0], 'base64').toString());
-            this._content = TransformUtil.toJSON(Buffer.from(array[1], 'base64').toString());
-            this._signature = Buffer.from(array[2], 'base64');
-        } catch (error) {
-            throw new OpenIdTokenInvalidError(error.message);
-        }
-    }
-
-    // --------------------------------------------------------------------------
-    //
-    //  Public Properties
-    //
-    // --------------------------------------------------------------------------
-
-    public get header(): IOpenIdTokenHeader {
-        return this._header;
-    }
-
-    public get signed(): string {
-        return this._signed;
-    }
-
-    public get content(): IOpenIdTokenContent {
-        return this._content;
-    }
-
-    public get signature(): Buffer {
-        return this._signature;
-    }
-
-    public get isExpired(): boolean {
-        return this.content.exp * DateUtil.MILLISECONDS_SECOND < Date.now();
-    }
-
-    public get value(): string {
-        return this._value;
-    }
-}
-
-export interface IOpenIdTokenHeader {
+export interface IKeycloakTokenHeader extends IOpenIdTokenHeader {
     alg: string;
-    typ: string;
-    kid: string;
-
-    [key: string]: any;
 }
 
-export type IOpenIdTokenContent = Record<string, any>;
+export interface IKeycloakContent extends IOpenIdTokenContent {
+    iat: number;
+    typ: string;
+    iss: string;
+    aud: string;
+    azp: string;
+    notBefore: number;
+}
