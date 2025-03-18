8074ef58b2c89c9129d581d415f72f09dca26e42
Renat Gubaev <gubaev.r@n-t.io>
2025-03-18 15:42:23 +0300

    auto commit

--- src/lib/IOpenIdResource.ts
+++ src/lib/IOpenIdResource.ts
@@ -1,7 +1,8 @@
 export type OpenIdResources = Map<string, IOpenIdResource>;
 
-export interface IOpenIdResource {
+export interface IOpenIdResource<T = Record<string, any>> {
     id: string;
     name: string;
     scopes: Array<string>;
+    attributes?: T;
 }
